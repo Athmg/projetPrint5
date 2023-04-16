feb40ac8746e48835929c61f54d5be80ff6e7e74
Atmg <athamidath@hotmail.com>
2023-04-17 00:45:09 +0200

    ajout du function selecteDot

--- assets/script.js
+++ assets/script.js
@@ -21,15 +21,14 @@ let i = 0;
 const nbImages = slides.length;
 
 const imageSlider = document.querySelector("#img");
+/**/
 const tagLines=document.querySelector("#banner-p")
+/**/
 const precedent = document.querySelector(".arrow_left");
 const suivant  = document.querySelector(".arrow_right");
 
-const elementBanner = document.querySelectorAll("#banner");
 const dotSelected = document.querySelector(".dot_selected")
 const dots = document.querySelectorAll(".dot");
-const ensembleDots=document.querySelector(".dots");
-
 
 suivant.addEventListener("click", 
 function () {
@@ -39,8 +38,8 @@ function () {
 	}
 	imageSlider.src = slides[i].image;
 	tagLines.innerHTML = slides[i].tagLine;
-	selectDot(dots,i);
-	console.log("image suivant");
+	selecteDot(dots,i);
+	/*console.log("image suivant");*/
 },true);
 
 precedent.addEventListener("click", function () {
@@ -50,14 +49,35 @@ precedent.addEventListener("click", function () {
 	}
 	imageSlider.src = slides[i].image;
 	tagLines.innerHTML = slides[i].tagLine;
-selectDot(dots,i);
-	console.log("image precedent");
-
+	selecteDot(dots,i);
+	
 }, true);
 
+/* 
+setInterval(afficherImage, 3000);
+ /**/
+
+function afficherImage() {
+	i++;
+	if (i > nbImages-1) {
+		i = 0;
+	}
+	imageSlider.src = slides[i].image;
+	tagLines.innerHTML = slides[i].tagLine;/**/
+	selecteDot(dots,i);
+}
 
 
-/***** Dots *****/
+function selecteDot(dots,i){
+	dots[i].classList.toggle("dot_selected");
+	dots.forEach(function(element, index){
+		if(index!==i){
+			element.classList.remove("dot_selected");
+		}
+	})
+}
+
+/***** Dots **
 let selected ="";
 let dot="";
 
@@ -68,30 +88,4 @@ dots.forEach(function(dot, index){
 	})
 })
 
-
-
-
-/* */
-setInterval("afficherImage()", 3000);
- /**/
-
-
-function afficherImage() {
-	i++;
-	if (i > nbImages-1) {
-		i = 0;
-	}
-	imageSlider.src = slides[i].image;
-}
-
-
-function selectDot(dots,i){
-	dots[i].classList.toggle("dot_selected");
-	dots.forEach(function(element, index){
-		if(index!==i){
-			element.classList.remove("dot_selected");
-		}
-	})
-}
-
-
+/**/
